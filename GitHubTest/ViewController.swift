bb73d0df33e44592ffc3cce6eb39e03f93becaf1
Jack W <jweeks@the-kaisers-booklet.local>
2018-04-06 10:24:55 -0500

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Jack Weeks on 4/6/18.
+//  Copyright © 2018 Jack Weeks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
